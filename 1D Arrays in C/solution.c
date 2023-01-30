b21e1d3465b2dc5a30f49ace12309abc3ab5d5f1
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2023-01-30 21:05:26 +0530

    solution for 1D array in C

---
+++ 1D Arrays in C/solution.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n,sum=0;
+    scanf("%d\n",&n);
+    int arr[n];
+    for (int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    printf("%d\n",sum);
+}
